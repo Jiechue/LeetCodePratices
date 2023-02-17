e6101890055760f544e90ac7a79e928c39104242
JunJie <q0490q0490@163.com>
2023-02-17 18:39:30 +0800

    有效的括号算法练习

---
+++ src/有效的括号.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class 有效的括号 {
+    private static final Map<Character,Character> map = new HashMap<Character,Character>(){{
+        put('{','}'); put('[',']'); put('(',')'); put('?','?');
+    }};
+    public static boolean isValid(String s) {
+        if(s.length() > 0 && !map.containsKey(s.charAt(0))) return false;
+        LinkedList<Character> stack = new LinkedList<Character>() {{ add('?'); }};
+        for(Character c : s.toCharArray()){
+            if(map.containsKey(c)) stack.addLast(c);
+            else if(map.get(stack.removeLast()) != c) return false;
+        }
+        return stack.size() == 1;
+    }
+
+    public static void main(String[] args) {
+        String s = "({}[])";
+        System.out.println(isValid(s));
+    }
+}
